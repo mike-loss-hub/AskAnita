c14df15ab1fae3a53685938c3ec4e2bef46e0d8c
michael-loss <129875370+michael-loss@users.noreply.github.com>
2025-02-12 08:29:30 -0600

    updated version to v3

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def main():
     # Sidebar setup
     with st.sidebar:
         st.image("AnitaMDorr.jpg", width=300, use_container_width=True)
-        st.title("Hello! I'm ANITA - v2")
+        st.title("Hello! I'm ANITA - v3")
 
         def on_enafocus_change():
             st.session_state.chat_handler = ChatHandler()
